0eb61c30f54f954d4b0476f97559a3a932a9923c
Camila Paganini <camilacpaganini@gmail.com>
2025-03-06 18:02:20 +0000

    Fato_pedidos atualizada

--- models/intermadiate/int__prep_pedidos.sql
+++ models/intermadiate/int__prep_pedidos.sql
@@ -1,22 +1,33 @@
 with 
     pedido as (
-        select * from {{ ref('stg__erp_pedido') }}
+        select 
+            p.*,
+            c.fk_loja_cliente,   -- Adicionando FK_LOJA
+            c.fk_territorio_cliente  -- Adicionando FK_TERRITÓRIO
+        from {{ ref('stg__erp_pedido') }} p
+        left join {{ ref('stg__erp_cliente') }} c 
+            on p.fk_cliente = c.pk_cliente
     )
+
 ,   pedido_detalhes as (
         select * from {{ ref('stg__erp_pedido_detalhes') }}
     )
+
 ,   pedido_motivo as (
         select 
             fk_pedido, 
-            array_agg(fk_motivo_venda) as motivos_venda
+            listagg(cast(fk_motivo_venda as varchar), ', ') within group (order by fk_motivo_venda) as motivos_venda
         from {{ ref('stg__erp_pedido_motivo') }}
         group by fk_pedido
     )
+
 ,   consolidado as (
         select 
             pedido_detalhes.pk_pedido_detalhe
         ,   pedido_detalhes.fk_pedido
         ,   pedido.fk_cliente
+        ,   pedido.fk_loja_cliente  
+        ,   pedido.fk_territorio_cliente  
         ,   pedido.fk_status_pedido
         ,   pedido.data_pedido
         ,   pedido.data_vencimento
@@ -32,12 +43,12 @@ with
         ,   pedido_detalhes.desconto
         ,   pedido_detalhes.preco_unitario * pedido_detalhes.quantidade as total_bruto
         ,   pedido_detalhes.preco_unitario * (1 - pedido_detalhes.desconto) * pedido_detalhes.quantidade as total_liquido
-        ,   cast((pedido.valor_frete / count(*) over(partition by pedido.pk_pedido)) as numeric(18,2)) as frete_rateado
+        ,   coalesce(pedido.valor_frete / nullif(count(*) over(partition by pedido.pk_pedido), 0), 0) as frete_rateado
         ,   case
                 when pedido_detalhes.desconto > 0 then true
                 else false
             end as teve_desconto
-        ,   coalesce(pedido_motivo.motivos_venda, array_construct('Desconhecido')) as fk_motivo_venda
+        ,   coalesce(pedido_motivo.motivos_venda, 'Desconhecido') as fk_motivo_venda
         from pedido_detalhes
         inner join pedido on pedido_detalhes.fk_pedido = pedido.pk_pedido
         left join pedido_motivo on pedido.pk_pedido = pedido_motivo.fk_pedido
@@ -45,7 +56,5 @@ with
 
 select *
 from consolidado
-
-
 
 
